84177ed0270d7f014909ce9869b8523cba9f1fcc
sowmyachan0107 <test.account@infobeans.com>
2022-12-20 10:32:07 +0530

    adding test3 branch

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,5 @@
-//const sum = require('./sum');
+const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-    let ans= 4
-  expect(ans).toBe(4);
+test('adds 1 + 2 to equal 3', () => { 
+  expect(sum(1,2)).toBe(4);
 });
